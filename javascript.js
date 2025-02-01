31603388dc4f025e71b5ee69339763976a15e19a
Thabza21 <82621614+Thabza21@users.noreply.github.com>
2025-02-01 17:05:17 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@
 
 
 function sendEmail(choice) {
-    fetch("valentine-production-39be.up.railway.app", {
+    fetch("https://valentine-production-39be.up.railway.app", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ choice: choice })
