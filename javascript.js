00a22d53057b6098b6e208d7cf1f42121d08b8b7
Thabza21 <82621614+Thabza21@users.noreply.github.com>
2025-02-01 15:57:16 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@
 
 
 function sendEmail(choice) {
-    fetch("https://thabza21.github.io/Valentine/", {
+    fetch("https://valentine-ucox.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ choice: choice })
